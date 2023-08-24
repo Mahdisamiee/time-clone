aea95148db6c3ca27f3d8e18855ce69ad274efd6
m_lind <md.samieenia@gmail.com>
2023-08-24 21:45:19 +0330

    fix time for TimeZone Clock

--- components/home/clock.tsx
+++ components/home/clock.tsx
@@ -1,15 +1,9 @@
 "use client";
-
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import moment from "moment-timezone";
-import ReactClock from "react-clock";
-import "react-clock/dist/Clock.css";
 import LiveClock from "react-live-clock";
 import Balancer from "react-wrap-balancer";
-import TimezoneSelect from "./timezone";
-
-import type { ITimezone } from "react-timezone-select";
 
 interface ClockProps {
   timeZone: string;
@@ -17,13 +11,7 @@ interface ClockProps {
 }
 
 const Clock: React.FC<ClockProps> = ({ timeZone, timeCity }) => {
-  const [fetchedDateTime, setFetchedDateTime] = useState<moment.Moment | null>(
-    null,
-  );
-
-  const handleTimezoneChange = (timeZone: string) => {
-    console.log(timeZone);
-  };
+  const [adjustedTime, setAdjustedTime] = useState<moment.Moment | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,11 +19,20 @@ const Clock: React.FC<ClockProps> = ({ timeZone, timeCity }) => {
         const response = await axios.get(
           `https://worldtimeapi.org/api/timezone/${timeZone}/${timeCity}`,
         );
-        const datetime = response.data.datetime;
-        const timeZoneName = response.data.timezone;
+        const serverTime = moment(response.data.datetime);
+        const localTime = moment();
+        const difference = serverTime.diff(localTime);
 
-        const zonedDate = moment.tz(datetime, timeZoneName);
-        setFetchedDateTime(zonedDate);
+        setAdjustedTime(moment().add(difference, "milliseconds"));
+
+        // Periodically adjust time to keep in sync with server
+        const interval = setInterval(() => {
+          setAdjustedTime((prevTime) =>
+            prevTime ? prevTime.add(1, "seconds") : null,
+          );
+        }, 1000);
+
+        return () => clearInterval(interval);
       } catch (error) {
         console.error("Failed to fetch the date and time:", error);
       }
@@ -45,36 +42,32 @@ const Clock: React.FC<ClockProps> = ({ timeZone, timeCity }) => {
   }, [timeZone, timeCity]);
 
   return (
-    <div>
-      {fetchedDateTime ? (
-        <div className="text-center">
-          <h1>
-            Exact Time For {timeZone} / {timeCity}
-          </h1>
-          <h1
-            className="text-10xl animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-7xl md:leading-[5rem]"
-            style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
-          >
-            <Balancer >
-              <LiveClock
-                format={"HH:mm:ss"}
-                ticking={true}
-                timezone={`${timeZone}/${timeCity}`}
-              />
-            </Balancer>
-          </h1>
-          <p
-            className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 md:text-xl"
-            style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
-          >
-            <Balancer>
-              {fetchedDateTime.format("dddd, MMMM D, YYYY h:mm:ss a z")}
-            </Balancer>
-          </p>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+    <div className="flex flex-col items-center text-center">
+      <h1>
+        Exact Time For {timeZone} / {timeCity}
+      </h1>
+      <h1
+        className="text-10xl animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-7xl md:leading-[5rem]"
+        style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
+      >
+        {adjustedTime && (
+          <LiveClock
+            date={adjustedTime.toISOString()}
+            format={"HH:mm:ss"}
+            ticking={true}
+            timezone={`${timeZone}/${timeCity}`}
+          />
+        )}
+      </h1>
+
+      <p
+        className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 md:text-xl"
+        style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+      >
+        <Balancer>
+          {adjustedTime && adjustedTime.format("dddd, MMMM D, YYYY")}
+        </Balancer>
+      </p>
     </div>
   );
 };
