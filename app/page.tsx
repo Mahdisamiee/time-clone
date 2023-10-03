b9d1decc4f4b8d8e8b105382172c7cb42fe19505
m_lind <md.samieenia@gmail.com>
2023-10-03 18:04:56 +0330

    change first page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-
 import Card from "@/components/home/card";
 import { Twitter, LinkedIn } from "@/components/shared/icons";
 import ComponentGrid from "@/components/home/component-grid";
@@ -9,64 +8,112 @@ import MainCalendar from "@/components/home/calendar";
 
 export default async function Home() {
   return (
-    <>
-      <div className="z-10 w-full max-w-xl px-5 xl:px-0">
-        <div
-          className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0"
-          style={{ animationDelay: "0.3s", animationFillMode: "forwards" }}
-        >
-          <LocalClock />
-        </div>
-
-        <a
-          href="https://www.linkedin.com/company/innofinityteam"
-          target="_blank"
-          rel="noreferrer"
-          className="innofinity_link mx-auto my-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200"
-        >
-          <LinkedIn className="h-5 w-5 text-[#1d9bf0] mx-1" />
-          <p className="text-md font-light text-[#1d9bf0]">
-            Introducing Innofinity
-          </p>
-        </a>
-      </div>
-      <div className="my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-5 xl:px-0">
-        {features.map(({ title, demo, large }) => (
-          <Card
-            key={title}
-            title={title}
-            demo={
-              title === "Beautiful, reusable components" ? (
-                <ComponentGrid />
-              ) : (
-                demo
-              )
-            }
-            large={large}
-          />
-        ))}
-      </div>
-    </>
+    <div className="z-10 w-full max-w-screen-lg px-5 xl:px-0">
+      <ul
+        className="mx-auto mt-6 grid animate-fade-up grid-cols-1 gap-20 px-5 opacity-0 md:grid-cols-6"
+        style={{ animationDelay: "0.3s", animationFillMode: "forwards" }}
+      >
+        {tools.map((tool) => {
+          return (
+            <li
+              className={`relative col-span-2 flex h-40 flex-col items-stretch justify-between overflow-hidden rounded-xl border border-gray-200 bg-white shadow-md`}
+            >
+              <a
+                href={tool.url}
+                target="_blank"
+                rel="noreferrer"
+                className="duration-450 flex h-40 max-w-full animate-fade-up flex-col items-center justify-center space-x-2 overflow-hidden rounded-xl px-7 py-2 text-3xl transition-colors hover:bg-blue-100"
+              >
+                <p className="mb-2 font-light text-[#1d9bf0]">{tool.title}</p>
+                <p>{tool.icon}</p>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
 
-const features = [
-  // {
-  //   title: "Beautiful, reusable components",
-  //   large: true,
-  // },
+const tools = [
   {
-    title: "ساعت دقیق سایر کشور ها",
-    large: true,
-    demo: <LiveClock />,
+    title: "وقت و زمان",
+    url: "/date-time",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="#1d9bf0"
+        className="h-12 w-12"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+    ),
   },
   {
-    title: "تقویم روزانه",
-    demo: <MainCalendar />,
+    title: "نقشه و فاصله",
+    url: "",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="#1d9bf0"
+        className="h-12 w-12"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M9 6.75V15m6-6v8.25m.503 3.498l4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 00-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0z"
+        />
+      </svg>
+    ),
   },
   {
-    title: "اوقات شرعی",
-    large: true,
-    demo: <ShariaTime />,
+    title: "تبدیل واحد",
+    url: "",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="#1d9bf0"
+        className="h-12 w-12"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: "وضعیت آب و هوا",
+    url: "/weather",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="#1d9bf0"
+        className="h-12 w-12"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M2.25 15a4.5 4.5 0 004.5 4.5H18a3.75 3.75 0 001.332-7.257 3 3 0 00-3.758-3.848 5.25 5.25 0 00-10.233 2.33A4.502 4.502 0 002.25 15z"
+        />
+      </svg>
+    ),
   },
 ];
