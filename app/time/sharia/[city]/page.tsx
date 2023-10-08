c36153a01aa15c6b611e43f448afa815698575dd
m_lind <md.samieenia@gmail.com>
2023-10-08 20:29:37 +0330

    make SEO for sharia/city pages

--- app/time/sharia/[city]/page.tsx
+++ app/time/sharia/[city]/page.tsx
@@ -8,7 +8,26 @@ export async function generateMetadata({
 }): Promise<Metadata> {
   const decodedCity = decodeURIComponent(params.city);
   return {
-    title: `اوقات شرعی ${decodedCity}`,
+    title: `اوقات شرعی ${decodedCity} | زمان طلوع و غروب آفتاب + زمان اذان`,
+    description: `اوقات شرعی دقیق شهر ${decodedCity} | طلوع و غروب دقیق آفتاب ${decodedCity} به همراه زمان دقیق اذان صبح، اذان ظهر و اذان مغرب | کیت ۳۶۵`,
+    keywords: [
+      `اوقات شرعی دقیق ${decodedCity}`,
+      `زمان دقیق طلوع و غروب ${decodedCity}`,
+      `زمان طلوع آفتاب ${decodedCity}`,
+      `طلوع آفتاب ${decodedCity}`,
+      `زمان غروب آفتاب ${decodedCity}`,
+      `غروب آفتاب ${decodedCity}`,
+      `زمان اذان صبح ${decodedCity}`,
+      `اذان صبح ${decodedCity}`,
+      `زمان اذان ظهر ${decodedCity}`,
+      `اذان ظهر ${decodedCity}`,
+      `اذان مغرب ${decodedCity}`,
+      `زمان اذان مغرب ${decodedCity}`,
+      `نیمه شب شرعی ${decodedCity}`,
+    ],
+    alternates: {
+      canonical : `https://kit365.ir/time/sharia/${decodedCity}`
+    }
   };
 }
 
