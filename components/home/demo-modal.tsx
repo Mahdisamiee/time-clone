f4db27de08417b4d53cde6690ab083c63e52d8d5
m_lind <md.samieenia@gmail.com>
2023-10-15 19:09:30 +0330

    full report on age modal

--- components/home/demo-modal.tsx
+++ components/home/demo-modal.tsx
@@ -23,8 +23,8 @@ const DemoModal = ({
 }) => {
   return (
     <Modal showModal={showDemoModal} setShowModal={setShowDemoModal}>
-      <div className="w-full overflow-hidden md:max-w-md md:rounded-2xl md:border md:border-gray-100 md:shadow-xl">
-        <div className="flex flex-col items-center justify-center space-y-3 bg-white px-4 py-6 pt-8 text-center md:px-16">
+      <div className="h-2/3 w-full overflow-hidden md:max-w-md md:rounded-2xl md:border md:border-gray-100 md:shadow-xl">
+        <div className="h-full flex flex-col items-center justify-center space-y-3 md:space-y-6 bg-white px-4 py-6 pt-8 text-center md:px-16">
           <a href="https://precedent.dev">
             <Image
               src="/logo.png"
@@ -34,23 +34,51 @@ const DemoModal = ({
               height={20}
             />
           </a>
-          <h3 className="font-display text-2xl font-bold">Precedent</h3>
-          <p className="text-xl text-gray-500">
+          <h3 className="text-3xl font-bold mb-10">سن شما مطابق تقویم شمسی</h3>
+          <hr className="w-full"/>
+          <p className="text-2xl text-gray-500">
             سن شما {details?.age.years} سال و {details?.age.months} ماه و{" "}
             {details?.age.days} روز است.
           </p>
-          <br className="my-2 w-full" />
+          <hr className="w-full  my-2"/>
           <p className="text-md text-gray-500">
             {persianToCalendars(
               details.selectedDay.year,
               details.selectedDay.month,
               details.selectedDay.day,
               {
-                toCal: "islamic",
+                toCal: "islamic-civil",
                 dateStyle: "full",
                 locale: "fa",
               },
             )}
+
+          </p>
+          <p className="text-md text-gray-500">
+            {persianToCalendars(
+              details.selectedDay.year,
+              details.selectedDay.month,
+              details.selectedDay.day,
+              {
+                toCal: "iso8601",
+                dateStyle: "full",
+                locale: "en",
+              },
+            )}
+
+          </p>
+          <p className="text-md text-gray-500">
+            {persianToCalendars(
+              details.selectedDay.year,
+              details.selectedDay.month,
+              details.selectedDay.day,
+              {
+                toCal: "iso8601",
+                dateStyle: "full",
+                locale: "fa",
+              },
+            )}
+
           </p>
         </div>
       </div>
