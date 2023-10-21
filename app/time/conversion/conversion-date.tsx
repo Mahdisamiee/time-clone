96acb0e7bddbd3c6d86396584f8d18d8f6cb06e9
m_lind <md.samieenia@gmail.com>
2023-10-21 11:40:09 +0330

    add modal

--- app/time/conversion/conversion-date.tsx
+++ app/time/conversion/conversion-date.tsx
@@ -8,9 +8,12 @@ import {
   convertGregorianToJalali,
   convertHijriToGregorian,
   convertJalaliToGregorian,
+  persianToCalendars,
 } from "@/lib/utils";
+import { useResultModal } from "./result-modal";
 
 const ConversionDate = () => {
+  const { setShowResultModal, ResultModal } = useResultModal();
   const [selectedType, setSelectedType] = useState<{
     value: number;
     label: string;
@@ -22,7 +25,7 @@ const ConversionDate = () => {
     day: number | null;
   }>({ year: null, month: null, day: null });
 
-  const [result, setResult] = useState<any>(null);
+  const [results, setResults] = useState<string[]>([]);
 
   const handleSelectType = (option: { value: number; label: string }) => {
     console.log("option---->", option);
@@ -39,47 +42,123 @@ const ConversionDate = () => {
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
+    let jalaaliDate;
+    let gregorianDate;
     // check to type and date not be empty
     if (!selectedType || !selectedDate) return;
     // base on type choose the function that handle date conversions
     switch (selectedType.value) {
+      // شمسی به میلادی و قمری
       case 1:
-        // شمسی به میلادی و قمری
-        let gregoriDate = convertJalaliToGregorian(selectedDate);
-        let hijriDate = convertGregorianToHijri(gregoriDate);
-        console.log("result : ", gregoriDate, hijriDate);
-        
+        if (selectedDate.year && selectedDate.month && selectedDate.day) {
+          let gregorianResult = persianToCalendars(
+            selectedDate.year,
+            selectedDate.month,
+            selectedDate.day,
+            {
+              toCal: "iso8601",
+              dateStyle: "full",
+              locale: "en",
+            },
+          );
+          let hijriResult = persianToCalendars(
+            selectedDate.year,
+            selectedDate.month,
+            selectedDate.day,
+            {
+              toCal: "islamic-civil",
+              dateStyle: "full",
+              locale: "fa",
+            },
+          );
+
+          setResults([gregorianResult, hijriResult]);
+          setShowResultModal(true);
+        }
+
         break;
+      // میلادی به شمسی و قمری
       case 2:
-        // میلادی به شمسی و قمری
-        
+        jalaaliDate = convertGregorianToJalali(selectedDate);
+        if (selectedDate.year && selectedDate.month && selectedDate.day) {
+          let jalaaliResult = persianToCalendars(
+            jalaaliDate.year,
+            jalaaliDate.month,
+            jalaaliDate.day,
+            {
+              toCal: "persian",
+              dateStyle: "full",
+              locale: "fa",
+            },
+          );
+          let hijriResult = persianToCalendars(
+            jalaaliDate.year,
+            jalaaliDate.month,
+            jalaaliDate.day,
+            {
+              toCal: "islamic-civil",
+              dateStyle: "full",
+              locale: "fa",
+            },
+          );
+
+          setResults([jalaaliResult, hijriResult]);
+          setShowResultModal(true);
+        }
         break;
 
+      // قمری به شمسی و میلادی
       case 3:
-        // قمری به شمسی و میلادی
-        
+        gregorianDate = convertHijriToGregorian(selectedDate);
+        jalaaliDate = convertGregorianToJalali(gregorianDate);
+        console.log(gregorianDate, jalaaliDate);
+        if (jalaaliDate.year && jalaaliDate.month && jalaaliDate.day) {
+          let jalaaliResult = persianToCalendars(
+            jalaaliDate.year,
+            jalaaliDate.month,
+            jalaaliDate.day,
+            {
+              toCal: "persian",
+              dateStyle: "full",
+              locale: "fa",
+            },
+          );
+          let gregorianResult = persianToCalendars(
+            jalaaliDate.year,
+            jalaaliDate.month,
+            jalaaliDate.day,
+            {
+              toCal: "iso8601",
+              dateStyle: "full",
+              locale: "en",
+            },
+          );
+
+          setResults([jalaaliResult, gregorianResult]);
+          setShowResultModal(true);
+        }
         break;
       default:
         break;
     }
-    // setResults and open Modal to show Results.
-    console.log("Selected Date", selectedDate);
-    console.log("Selected Type", selectedType);
-    // console.log(convertGregorianToHijri(selectedDate));
-    // console.log(convertIslamicToGregorian(selectedDate))
   };
 
   return (
-    <div className="flex flex-col items-center justify-around gap-5 bg-gray-200">
+    <div className="flex flex-col items-center justify-around gap-5 py-10">
       <SelectType selectedType={selectedType} onSelectType={handleSelectType} />
       <DatePicker
         type={selectedType?.value}
         date={selectedDate}
         onChangeDate={handleDateChange}
       />
-      <button onClick={handleSubmit} type="submit">
-        Click
+      <button
+        onClick={handleSubmit}
+        type="submit"
+        className="bg-blue-500 hover:bg-blue-600 focus:bg-blue-600 active:bg-blue-700 mb-2 block w-full sm:w-3/4 rounded px-6 pb-2 pt-2.5 text-md font-md uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+      >
+        محاسبه
       </button>
+      {ResultModal({ results })}
     </div>
   );
 };
---
+++ app/time/conversion/result-modal.tsx
@@ -0,0 +1,81 @@
+import Modal from "@/components/shared/modal";
+import {
+  useState,
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useMemo,
+} from "react";
+import Image from "next/image";
+import { persianToCalendars } from "@/lib/utils";
+
+const ResultModal = ({
+  showResultModal,
+  setShowResultModal,
+  details,
+}: {
+  showResultModal: boolean;
+  setShowResultModal: Dispatch<SetStateAction<boolean>>;
+  details: {
+    results: string[];
+  };
+}) => {
+  return (
+    <Modal showModal={showResultModal} setShowModal={setShowResultModal}>
+      <div className="h-2/3 w-full overflow-hidden md:max-w-md md:rounded-2xl md:border md:border-gray-100 md:shadow-xl">
+        <div className="flex h-full flex-col items-center justify-center space-y-3 bg-white px-4 py-6 pt-8 text-center md:space-y-6 md:px-16">
+          <a href="https://precedent.dev">
+            <Image
+              src="/logo.png"
+              alt="Precedent Logo"
+              className="h-10 w-10 rounded-full"
+              width={20}
+              height={20}
+            />
+          </a>
+
+          <>
+            <h3 className="mb-10 text-3xl font-bold">
+              سن شما مطابق تقویم شمسی
+            </h3>
+            <hr className="w-full" />
+            <p className="text-2xl text-gray-500">
+              here we put more info!
+            </p>
+            <hr className="my-2  w-full" />
+            {details.results &&
+              details.results.map((result) => (
+                <p className="text-md text-gray-500">{result}</p>
+              ))}
+          </>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export function useResultModal() {
+  const [showResultModal, setShowResultModal] = useState(false);
+
+  const ResultModalCallback = useCallback(
+    (details: { results: string[] }) => {
+      return (
+        <ResultModal
+          showResultModal={showResultModal}
+          setShowResultModal={setShowResultModal}
+          details={details}
+        />
+      );
+    },
+    [showResultModal, setShowResultModal],
+  );
+
+  return useMemo(
+    () => ({
+      setShowResultModal,
+      ResultModal: (details: { results: string[] }) =>
+        ResultModalCallback(details),
+    }),
+    [setShowResultModal, ResultModalCallback],
+  );
+}
