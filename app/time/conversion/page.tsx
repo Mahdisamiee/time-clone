9a955b3a623a9cacbb3ac8099b8abbb1597e31e3
m_lind <md.samieenia@gmail.com>
2023-10-22 16:47:03 +0330

    add seo to conversion / create local-navbar

--- app/time/conversion/page.tsx
+++ app/time/conversion/page.tsx
@@ -6,41 +6,34 @@ import MainCalendar from "@/components/time/calendar";
 import { Metadata } from "next";
 import Link from "next/link";
 import ConversionDate from "./conversion-date";
+import LocalNavbar from "@/components/shared/local-navbar";
 
 export const metadata: Metadata = {
-  title: "ساعت و زمان",
+  title: "تبدیل تاریخ شمسی میلادی قمری",
   description:
-    "ابزارهای اعلام وقت و زمان، اوقات شرعی شهرها و تاریخ دقیق، تعیین و تبدیل انواع تقویم ها و زمان",
+    "تبدیل تاریخ شمسی به میلادی ، میلادی به شمسی ، تبدیل تاریخ تولد به میلادی ، مبدل تاریخ و تبدیل تقویم به شمسی و میلادی و قمری",
   keywords: [
-    "اوقات شرعی دقیق",
-    "تاریخ و ساعت دقیق",
-    "تقویم روز",
     "تبدیل تاریخ",
-    "محاسبه سن و فاصله دو تاریخ",
-    "",
+    "تبدیل تاریخ تولد",
+    "تبدیل تاریخ شمسی به میلادی",
+    "تبدیل تاریخ تولد به قمری",
+    "تبدیل تاریخ به قمری",
+    "تبدیل تاریخ به میلادی",
+    "تبدیل تاریخ میلادی به شمسی",
+    "تبدیل تاریخ میلادی به قمری",
+    "تبدیل تاریخ به قمری",
+    "تاریخ امروز به میلادی",
+    "تاریخ امروز به قمری",
+    "امروز به قمری",
+    "امروز به شمسی",
+    "امروز به میلادی",
   ],
 };
 
 const Conversion = () => {
   return (
     <>
-      <div
-        className={`z-10 grid grid-cols-1 gap-5 md:grid-cols-${timeParts.length}`}
-      >
-        {timeParts.map((part) => {
-          return (
-            <Link
-              prefetch={true}
-              key={`${part.url}`}
-              className="w-18 flex items-center justify-center rounded-md border border-gray-300 px-2 py-1 transition-all duration-100 hover:border-blue-500 focus:outline-none active:bg-gray-100"
-              href={part.url}
-            >
-              <p className="text-gray-600">{part.title}</p>
-            </Link>
-          );
-        })}
-        <hr className={`col-span-6 mb-10 mt-2 w-full`} />
-      </div>
+      <LocalNavbar items={timeParts} />
 
       <div className="z-10 w-full max-w-3xl px-5 xl:px-0">
         {/* here we add our first client component */}
@@ -53,6 +46,10 @@ export default Conversion;
 
 const timeParts = [
   {
+    title: "تاریخ امروز",
+    url: "/time",
+  },
+  {
     title: "تقویم ایران",
     url: "time/calendar",
   },
@@ -61,36 +58,7 @@ const timeParts = [
     url: "time/age",
   },
   {
-    title: "اوقات شرعی",
-    url: "time/sharia",
-  },
-  {
-    title: "تبدیل تاریخ",
-    url: "time/conversion",
-  },
-  {
-    title: "ساعت کشورها",
-    url: "time/worldclock",
-  },
-  {
     title: "فاصله دو تاریخ",
     url: "time/diff",
-  },
-];
-
-const cards = [
-  // {
-  //   title: "تقویم روزانه",
-  //   demo: <MainCalendar />,
-  // },
-  {
-    title: "اوقات شرعی",
-    large: true,
-    demo: <ShariaTime />,
-  },
-  {
-    title: "ساعت دقیق سایر کشور ها",
-    large: false,
-    demo: <LiveClock />,
   },
 ];
