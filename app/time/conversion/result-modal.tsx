8e3bfec77e8b531b77dc4d74c9d976b71a63ac34
m_lind <md.samieenia@gmail.com>
2023-10-24 21:35:36 +0330

    fix modal views

--- app/time/conversion/result-modal.tsx
+++ app/time/conversion/result-modal.tsx
@@ -24,29 +24,28 @@ const ResultModal = ({
     <Modal showModal={showResultModal} setShowModal={setShowResultModal}>
       <div className="h-2/3 w-full overflow-hidden md:max-w-md md:rounded-2xl md:border md:border-gray-100 md:shadow-xl">
         <div className="flex h-full flex-col items-center justify-center space-y-3 bg-white px-4 py-6 pt-8 text-center md:space-y-6 md:px-16">
-          <a href="https://precedent.dev">
-            <Image
-              src="/logo.png"
-              alt="Precedent Logo"
-              className="h-10 w-10 rounded-full"
-              width={20}
-              height={20}
-            />
-          </a>
-
           <>
-            <h3 className="mb-10 text-3xl font-bold">
-              سن شما مطابق تقویم شمسی
+            <h3 className="my-5 text-2xl font-bold">
+              تاریخ انتخابی برابر با سایر تقویم ها
             </h3>
-            <hr className="w-full" />
-            <p className="text-2xl text-gray-500">
-              here we put more info!
-            </p>
-            <hr className="my-2  w-full" />
+            <hr className="mb-10 mt-2 h-0.5 w-full rounded bg-blue-400" />
             {details.results &&
               details.results.map((result) => (
-                <p key={result} className="text-md text-gray-500">{result}</p>
+                <p key={result} className="text-lg sm:text-xl text-gray-700">
+                  {result}
+                </p>
               ))}
+            <hr className="my-2 mb-10 mt-2 h-0.5 w-full rounded bg-blue-400" />
+            <p className="text-2xl text-gray-500">here we put more info!</p>
+            <a href="https://precedent.dev">
+              <Image
+                src="/logo.png"
+                alt="Precedent Logo"
+                className="h-10 w-10 rounded-full"
+                width={20}
+                height={20}
+              />
+            </a>
           </>
         </div>
       </div>
