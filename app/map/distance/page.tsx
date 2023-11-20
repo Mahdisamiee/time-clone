553587272edc6cb369dac86728f30e4fba780321
m_lind <md.samieenia@gmail.com>
2023-11-20 09:17:56 +0330

    add Seo materials to /map/distance

--- app/map/distance/page.tsx
+++ app/map/distance/page.tsx
@@ -1,7 +1,7 @@
 // MapContainer.tsx
 import LocalNavbar from "@/components/shared/local-navbar";
 import MapContainer from "./distance-map";
-
+import { Metadata } from "next";
 
 const timeParts = [
   {
@@ -17,6 +17,27 @@ const timeParts = [
     url: "/time/conversion",
   },
 ];
+
+export const metadata: Metadata = {
+  title: "فاصله شهر‌ها و راههای شهرهای ایران و جهان",
+  description:
+    "محاسبه فاصله شهرها و مسافت راههای ایران ، فاصله استانبول کربلا تهران اصفهان شیراز مشهد تبریز و تمام شهرها تا یکدیگر ، مسافت یاب بین شهری",
+  keywords: [
+    "فاصله تهران تا اصفهان ",
+    "فاصله وان تا استانبول ",
+    "فاصله تهران تا مشهد ",
+    "فاصله تهران تا تبریز ",
+    "فاصله تهران تا شیراز ",
+    "فاصله تهران تا رشت ",
+    "فاصله اصفهان تا شیراز ",
+    "فاصله وان تا ترابزون ",
+    "فاصله وان تا ازمیر ",
+    "فاصله تهران تا یزد",
+  ],
+  alternates: {
+    canonical: `https://kit365.ir/map/distance`,
+  },
+};
 
 const MapPage: React.FC = () => {
   return (
