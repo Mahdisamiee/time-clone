f55bdbb675650f22a7ba1ed49cfa1cd331cbdead
m_lind <md.samieenia@gmail.com>
2023-12-29 09:54:57 +0330

    clean code

--- app/calc/conversion-unit-form.tsx
+++ app/calc/conversion-unit-form.tsx
@@ -8,6 +8,7 @@ import {
   postConversion,
   validateForm,
 } from "services/unit-services";
+import { createUnitOptions, defaultUnitsForMode } from "./unit-config";
 
 const ConversionUnitForm = ({
   unitMode,
@@ -158,26 +159,4 @@ const ConversionUnitForm = ({
 
 export default ConversionUnitForm;
 
-const createUnitOptions = (units: string[]) => {
-  return units.map((unit) => ({
-    value: unit,
-    label: `${unit.toLocaleUpperCase()}`,
-  }));
-};
 
-const defaultUnitsForMode: UnitDefaults = {
-  length: { fromUnit: "meter", toUnit: "mile" },
-  area: { fromUnit: "squaremeter", toUnit: "squarekilometer" },
-  mass: { fromUnit: "kilogram", toUnit: "gram" },
-  time: { fromUnit: "second", toUnit: "milisecond" },
-  "data-transfer": { fromUnit: "bitpersecond", toUnit: "kilobitpersecond" },
-  "digital-storage": { fromUnit: "byte", toUnit: "kilobyte" },
-  energy: { fromUnit: "joule", toUnit: "kilojoule" },
-  volume: { fromUnit: "liter", toUnit: "mililiter" },
-  speed: { fromUnit: "meterpersecond", toUnit: "kilometerperhour" },
-  pressure: { fromUnit: "pascal", toUnit: "bar" },
-  // ... other modes
-};
-
-// const [fromUnit, setFromUnit] = useState(defaultUnitsForMode[unitMode].fromUnit);
-// const [toUnit, setToUnit] = useState(defaultUnitsForMode[unitMode].toUnit);
---
+++ app/calc/unit-config.ts
@@ -0,0 +1,22 @@
+import { UnitDefaults } from "@/lib/models/calc";
+
+export const defaultUnitsForMode: UnitDefaults = {
+  length: { fromUnit: "meter", toUnit: "mile" },
+  area: { fromUnit: "squaremeter", toUnit: "squarekilometer" },
+  mass: { fromUnit: "kilogram", toUnit: "gram" },
+  time: { fromUnit: "second", toUnit: "milisecond" },
+  "data-transfer": { fromUnit: "bitpersecond", toUnit: "kilobitpersecond" },
+  "digital-storage": { fromUnit: "byte", toUnit: "kilobyte" },
+  energy: { fromUnit: "joule", toUnit: "kilojoule" },
+  volume: { fromUnit: "liter", toUnit: "mililiter" },
+  speed: { fromUnit: "meterpersecond", toUnit: "kilometerperhour" },
+  pressure: { fromUnit: "pascal", toUnit: "bar" },
+};
+
+
+export const createUnitOptions = (units: string[]) => {
+  return units.map((unit) => ({
+    value: unit,
+    label: `${unit.toLocaleUpperCase()}`,
+  }));
+};
