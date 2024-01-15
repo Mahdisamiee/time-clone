119ef66a12b3bcd396a56135fb29ee2abd84e607
m_lind <md.samieenia@gmail.com>
2024-01-15 22:43:45 +0330

    use shared files

---
+++ app/currency/mesghal/page.tsx
@@ -0,0 +1,22 @@
+import SimpleCard from "@/components/home/simple-card";
+import GoldCalculateForm from "../shared/gold-calculate-form";
+import GoldLivePurchase from "../shared/gold-live-purchase";
+
+const GoldPage = () => {
+  return (
+    <div className="grid h-full w-full grid-cols-1 justify-items-stretch gap-10 px-10 md:grid-cols-8 ">
+      <div className="col-span-1 md:col-span-3">
+        <SimpleCard>
+          <GoldLivePurchase title="قیمت لحظه‌ای مثقال طلا" />
+        </SimpleCard>
+      </div>
+      <div className="col-span-1 md:col-span-5">
+        <SimpleCard>
+          <GoldCalculateForm />
+        </SimpleCard>
+      </div>
+    </div>
+  );
+};
+
+export default GoldPage;
