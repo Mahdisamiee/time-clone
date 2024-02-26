7fef04bf667776eb9674f6e8c27b330aa145d26f
m_lind <md.samieenia@gmail.com>
2024-02-26 22:55:31 +0330

    edit static params

--- app/calc/[mode]/[option]/page.tsx
+++ app/calc/[mode]/[option]/page.tsx
@@ -4,51 +4,64 @@ import { Metadata } from "next";
 import { fetchUnitOptions } from "services/unit-services";
 import ConversionUnitForm from "../../conversion-unit-form";
 
-// export async function generateStaticParams({
-//   params: { mode },
-// }: {
-//   params: { mode: UnitMode };
-// }) {
-//   const result = await fetchUnitOptions(mode);
-//   const units = result.units;
+export async function generateStaticParams({
+  params: { mode },
+}: {
+  params: { mode: UnitMode };
+}) {
+  try {
+    const result = await fetchUnitOptions(mode);
+    const units = result.units;
 
-//   const arr = units!.map((unit: any) => {
-//     return units!.map((unit2: any) => {
-//       return { option: unit.value + "-" + unit2.value };
-//     });
-//   });
-//   return arr.flat(1);
-// }
+    return units!
+      .map((unit: any) => {
+        return units!.map((unit2: any) => {
+          return { option: unit.value + "-" + unit2.value };
+        });
+      })
+      .flat(1);
+  } catch (error) {
+    return [];
+  }
+}
 
 export async function generateMetadata({
   params: { mode, option },
 }: {
   params: { mode: UnitMode; option: string };
 }): Promise<Metadata> {
-  const result = await fetchUnitOptions(mode);
-  const units = result.units;
-  let titleParts = option.split("-to-");
-  let computedKeywords = units!.map(
-    (unit) =>
-      `تبدیل ${titleParts[0]}, تبدیل مقدار ${titleParts[0]}, تبدیل ${titleParts[1]}, تبدیل مقدار ${titleParts[1]}`,
-  );
-  let complicatedKeywords = units!
-    .map((unit1) => {
-      return units!.map(
-        (unit2) =>
-          `تبدیل ${titleParts[0]} به ${titleParts[1]} , تبدیل مقدار ${titleParts[0]} به ${titleParts[1]}, تبدیل واحد ${titleParts[0]} به ${titleParts[1]}`,
-      );
-    })
-    .flat(1);
+  try {
+    const result = await fetchUnitOptions(mode);
+    const units = result.units;
+    let titleParts = option.split("-to-");
+    let computedKeywords = units!.map(
+      (unit) =>
+        `تبدیل ${titleParts[0]}, تبدیل مقدار ${titleParts[0]}, تبدیل ${titleParts[1]}, تبدیل مقدار ${titleParts[1]}`,
+    );
+    let complicatedKeywords = units!
+      .map((unit1) => {
+        return units!.map(
+          (unit2) =>
+            `تبدیل ${titleParts[0]} به ${titleParts[1]} , تبدیل مقدار ${titleParts[0]} به ${titleParts[1]}, تبدیل واحد ${titleParts[0]} به ${titleParts[1]}`,
+        );
+      })
+      .flat(1);
 
-  return {
-    title: `محاسبه و تبدیل ${titleParts[0]} به ${titleParts[1]}`,
-    description: `تبدیل انواع ${mode} | تبدیل دقیق واحد های  ${mode} | تبدیل واحد ${titleParts[0]} به ${titleParts[1]} | harchi.app`,
-    keywords: [...computedKeywords, ...complicatedKeywords],
-    alternates: {
-      canonical: `https://harchi.app/time/${mode}/${option}`,
-    },
-  };
+    return {
+      title: `محاسبه و تبدیل ${titleParts[0]} به ${titleParts[1]}`,
+      description: `تبدیل انواع ${mode} | تبدیل دقیق واحد های  ${mode} | تبدیل واحد ${titleParts[0]} به ${titleParts[1]} | harchi.app`,
+      keywords: [...computedKeywords, ...complicatedKeywords],
+      alternates: {
+        canonical: `https://harchi.app/time/${mode}/${option}`,
+      },
+    };
+  } catch (error) {
+    return {
+      alternates: {
+        canonical: `https://harchi.app/time/${mode}/${option}`,
+      },
+    };
+  }
 }
 
 const UnitOptions = ({
--- app/calc/[mode]/page.tsx
+++ app/calc/[mode]/page.tsx
@@ -14,32 +14,42 @@ const UnitHome = ({ params }: { params: { mode: UnitMode } }) => {
 };
 export default UnitHome;
 
-// export async function generateMetadata({
-//   params: { mode },
-// }: {
-//   params: { mode: UnitMode };
-// }): Promise<Metadata> {
-//   const result = await fetchUnitOptions(mode);
-//   const units = result.units;
-//   let computedKeywords = units!.map(
-//     (unit) =>
-//       `تبدیل ${unit.flabel}, تبدیل مقدار ${unit.flabel}, تبدیل ${unit.label}, تبدیل مقدار ${unit.label}`,
-//   );
-//   let complicatedKeywords = units!
-//     .map((unit1) => {
-//       return units!.map(
-//         (unit2) =>
-//           `تبدیل ${unit1.flabel} به ${unit2.flabel} , تبدیل مقدار ${unit1.flabel} به ${unit2.flabel}, تبدیل ${unit1.label} به ${unit2.label}, تبدیل مقدار ${unit1.label} به ${unit2.label}`,
-//       );
-//     })
-//     .flat(1);
+export async function generateMetadata({
+  params: { mode },
+}: {
+  params: { mode: UnitMode };
+}) {
+  try {
+    const result = await fetchUnitOptions(mode);
+    const units = result.units;
+    let computedKeywords = units!.map(
+      (unit) =>
+        `تبدیل ${unit.flabel}, تبدیل مقدار ${unit.flabel}, تبدیل ${unit.label}, تبدیل مقدار ${unit.label}`,
+    );
+    let complicatedKeywords = units!
+      .map((unit1) => {
+        return units!.map(
+          (unit2) =>
+            `تبدیل ${unit1.flabel} به ${unit2.flabel} , تبدیل مقدار ${unit1.flabel} به ${unit2.flabel}, تبدیل ${unit1.label} به ${unit2.label}, تبدیل مقدار ${unit1.label} به ${unit2.label}`,
+        );
+      })
+      .flat(1);
 
-//   return {
-//     title: `محاسبه و تبدیل واحد‌های ${mode}`,
-//     description: `تبدیل انواع ${mode} | تبدیل دقیق واحد های  ${mode} | harchi.app`,
-//     keywords: [...computedKeywords, ...complicatedKeywords],
-//     alternates: {
-//       canonical: `https://harchi.app/calc/${mode}`,
-//     },
-//   };
-// }
+    return {
+      title: `محاسبه و تبدیل واحد‌های ${mode}`,
+      description: `تبدیل انواع ${mode} | تبدیل دقیق واحد های  ${mode} | harchi.app`,
+      keywords: [...computedKeywords, ...complicatedKeywords],
+      alternates: {
+        canonical: `https://harchi.app/calc/${mode}`,
+      },
+    };
+  } catch (error) {
+    return {
+      title: `محاسبه و تبدیل واحد‌های ${mode}`,
+      description: `تبدیل انواع ${mode} | تبدیل دقیق واحد های  ${mode} | harchi.app`,
+      alternates: {
+        canonical: `https://harchi.app/calc/${mode}`,
+      },
+    };
+  }
+}
