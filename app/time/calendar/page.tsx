bfd4af2bfb5e3ea80ac13edbe5e5621975dba60d
m_lind <md.samieenia@gmail.com>
2024-03-03 23:12:34 +0330

    fix bug

--- app/time/calendar/page.tsx
+++ app/time/calendar/page.tsx
@@ -1,27 +1,27 @@
-// import { Metadata } from "next";
+import { Metadata } from "next";
 
-// export const metadata: Metadata = {
-//   title: "تقویم ایران + مناسبتها و تعطیلات",
-//   description:
-//     "ابزارهای اعلام وقت و زمان، اوقات شرعی شهرها و تاریخ دقیق، تعیین و تبدیل انواع تقویم ها و زمان",
-//   keywords: [
-//     "اوقات شرعی دقیق",
-//     "تاریخ و ساعت دقیق",
-//     "تقویم روز",
-//     "تبدیل تاریخ",
-//     "محاسبه سن و فاصله دو تاریخ",
-//     "",
-//   ],
+export const metadata: Metadata = {
+  title: "تقویم ایران + مناسبتها و تعطیلات",
+  description:
+    "ابزارهای اعلام وقت و زمان، اوقات شرعی شهرها و تاریخ دقیق، تعیین و تبدیل انواع تقویم ها و زمان",
+  keywords: [
+    "اوقات شرعی دقیق",
+    "تاریخ و ساعت دقیق",
+    "تقویم روز",
+    "تبدیل تاریخ",
+    "محاسبه سن و فاصله دو تاریخ",
+    "",
+  ],
 
-//   alternates: {
-//     canonical: `https://harchi.app/time/age`,
-//   },
-// };
+  alternates: {
+    canonical: `https://harchi.app/time/age`,
+  },
+};
 
-// export default function Calendar() {
-//   return (
-//     <>
-//       <p>this is calendar place</p>
-//     </>
-//   );
-// }
+export default function Calendar() {
+  return (
+    <>
+      <p>this is calendar place</p>
+    </>
+  );
+}
