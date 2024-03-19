3583192f906a0c6950c79fed29a5272bcd60423c
m_lind <md.samieenia@gmail.com>
2024-03-19 12:07:21 +0330

    merge and fix font issue

--- app/fonts/index.ts
+++ app/fonts/index.ts
@@ -5,14 +5,20 @@ import { Vazirmatn } from "next/font/google";
 export const sfPro = localFont({
   src: "./SF-Pro-Display-Medium.otf",
   variable: "--font-sf",
+  display: "swap",
+  adjustFontFallback: false,
 });
 
 export const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
+  display: "swap",
+  adjustFontFallback: false,
 });
 export const vazirmatn = Vazirmatn({
   variable: "--font-vazirmatn",
   weight: ["700"],
   subsets: ["latin", "arabic"],
+  display: "swap",
+  adjustFontFallback: false,
 });
